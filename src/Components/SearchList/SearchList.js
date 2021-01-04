4dac43d9f7fd1dec72694a9093846e6de478c73e
John Byrne <jbyrne87@gmail.com>
2021-01-04 15:10:54 -0600

    added search list name to top of search list

--- src/Components/SearchList/SearchList.js
+++ src/Components/SearchList/SearchList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MyMashCard from '../MyMashCard/MyMashCard'
+import './SearchList.css'
 
 export default function SearchList(props) {
   let mashByGame = props.mashes.filter((mash) =>
@@ -10,7 +11,7 @@ export default function SearchList(props) {
   return (
     <>
       <section className='search-list'>
-        <h2>Mash List for {props.game_title}</h2>
+        <h2 className='search-title'>Mash List for {props.match.params.gameName}</h2>
         <span className='results'>
           {mashByGame.map((mash) => (
             <MyMashCard key={mash.id} {...mash} mashId={mash.id} />
